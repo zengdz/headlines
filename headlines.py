e547906ef91d2f11368786cd7cd638493b1f34d7
zengdz <zeng.zhen@foxmail.com>
2016-09-27 09:51:06 +0800

    简单展示一条新闻(1)
    使用HTML简单显示一个RSSFeed的首条新闻信息

--- headlines.py
+++ headlines.py
@@ -1,12 +1,25 @@
 #2016/09/26
 
 from flask import Flask
+import feedparser
 
 app = Flask(__name__)
 
+BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+
 @app.route("/")
 def get_news():
-    return "no news is good news"
+    feed = feedparser.parse(BBC_FEED)
+    first_article = feed['entries'][0]
+    return """<html>
+    <body>
+    <h1> BBC Headlines </h1>
+    <b>{0}</b> <br/>
+    <i>{1}</i> <br/>
+    <p>{2}</p> <br/>
+    </body>
+    </html>""".format(first_article.get("title"), first_article.
+    get("published"), first_article.get("summary"))
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
