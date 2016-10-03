e83d5899e10a5e70c90e7c92f4f7d7681dc10a95
zengdz <zeng.zhen@foxmail.com>
2016-10-03 23:10:54 +0800

    初步使用模板(template1)
    更改了可用的RSS源，中文的不能处理。初步使用模板

--- headlines.py
+++ headlines.py
@@ -1,29 +1,21 @@
 #2016/09/26
+#2016/10/03
 
 from flask import Flask
 import feedparser
+from flask import render_template
 
 app = Flask(__name__)
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
-             '163': 'http://news.163.com/special/00011K6L/rss_newstop.xml',
-             'chinadaily': 'http://www.chinadaily.com.cn/rss_c/kjhlw.xml',
-             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+             'engadget': 'https://www.engadget.com/rss.xml'}
 
 @app.route("/")
 @app.route("/<publication>")
 def get_news(publication = 'bbc'):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
-    return """<html>
-    <body>
-    <h1> BBC Headlines </h1>
-    <b>{0}</b> <br/>
-    <i>{1}</i> <br/>
-    <p>{2}</p> <br/>
-    </body>
-    </html>""".format(first_article.get("title"), first_article.
-    get("published"), first_article.get("summary"))
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
