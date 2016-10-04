dc73d66c1bb54ca8be275f5a92b4f16fd54a61d5
zengdz <zeng.zhen@foxmail.com>
2016-10-04 14:43:00 +0800

    使用 Jinjia 逻辑的 for 循环(JinjiaFor)
    为了显示多条新闻信息，需要使用循环逐个处理，注意格式，也需要代码知识循环结束。<hr />表示每条新闻之间水平线隔开

--- headlines.py
+++ headlines.py
@@ -12,8 +12,7 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 @app.route("/<publication>")
 def get_news(publication = 'bbc'):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    return render_template("home.html", article = first_article)
+    return render_template("home.html", articles = feed['entries'])
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
