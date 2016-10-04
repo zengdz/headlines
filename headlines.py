0cdbc51f41799b8646cc6a4905390edcada13efe
zengdz <zeng.zhen@foxmail.com>
2016-10-04 11:14:16 +0800

    初步使用 Jinjia 语法(JinjiaPre)
    开始使用 Jinjia 语法给显示的内容传递动态新闻信息，前后端通过相同的变量名来指定

--- headlines.py
+++ headlines.py
@@ -15,7 +15,10 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication = 'bbc'):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
-    return render_template("home.html")
+    return render_template("home.html",
+                           title = first_article.get("title"),
+                           published=first_article.get("published"),
+                           summary=first_article.get("summary"))
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
