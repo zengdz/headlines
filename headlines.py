1aee6c8bad2c2fd9bf89081ccd4145d166634f99
zengdz <zeng.zhen@foxmail.com>
2016-10-10 14:47:31 +0800

    添加city输入以及优化函数分工(multi-func)
    前端添加一个输入城市的输入框，后端处理两个用户数据的输入，分别使用两个函数获取新闻和天气，然后在主页函数中统一调用，使得程序结构有序。

--- headlines.py
+++ headlines.py
@@ -1,7 +1,7 @@
-
-import json
-import urllib2
-import urllib 
+#For Python2.X
+import json # built-in mudule
+import urllib2 # built-in mudule
+import urllib  # built-in mudule
 import feedparser
 from flask import Flask
 from flask import render_template
@@ -12,10 +12,35 @@ app = Flask(__name__)
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'engadget': 'https://www.engadget.com/rss.xml'}
 
+DEFAULTS = {'publication':'bbc', 'city':'London,UK'}# default setting when none input
+
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=ffbf9455a5a14c28bef75a03a8a7b308"#appid=a private API KEY
+
+@app.route("/")
+def home():
+    # get customized headlines, based on user input or default
+    publication = request.args.get("publication")
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    # get customized weather based on user input or default
+    city = request.args.get("city")
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    return render_template("home.html", articles = articles, weather=weather)
+
+def get_news(query): 
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = DEFAULTS['publication']
+    else:
+        publication = query.lower()
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    return feed['entries']
+    
 def get_weather(query):
-    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=ffbf9455a5a14c28bef75a03a8a7b308"#appid=a private API KEY
-    query = urllib.quote(query)#replace space in city's name with %20,url can't have space
-    url = api_url.format(query)
+    query = urllib.quote(query)# replace space in city's name with %20,url can't have space
+    url = WEATHER_URL.format(query)
     data = urllib2.urlopen(url).read()
     parsed = json.loads(data)
     weather = None
@@ -23,17 +48,6 @@ def get_weather(query):
         weather = {"description":parsed["weather"][0]["description"],
                     "temperature":parsed["main"]["temp"],"city":parsed["name"]}
     return weather
-
-@app.route("/")
-def get_news():
-    query = request.args.get("publication")
-    if not query or query.lower() not in RSS_FEEDS:
-        publication = "bbc"
-    else:
-        publication = query.lower()
-    feed = feedparser.parse(RSS_FEEDS[publication])
-    weather = get_weather("London,UK")
-    return render_template("home.html", articles = feed['entries'], weather=weather)
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
