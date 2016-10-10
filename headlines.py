448f1576967bd4d7b2f6d15262395ac59fac6a18
zengdz <zeng.zhen@foxmail.com>
2016-10-10 11:19:36 +0800

    后端获取天气API的json数据(weather)
    这是Python2版本的代码，所以同时使用了urllib和urllib2，获取天气API网址的json数据然后解析，把得到的数据处理后，传递给前端进行显示。

--- headlines.py
+++ headlines.py
@@ -1,4 +1,7 @@
 
+import json
+import urllib2
+import urllib 
 import feedparser
 from flask import Flask
 from flask import render_template
@@ -9,6 +12,18 @@ app = Flask(__name__)
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'engadget': 'https://www.engadget.com/rss.xml'}
 
+def get_weather(query):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=ffbf9455a5a14c28bef75a03a8a7b308"#appid=a private API KEY
+    query = urllib.quote(query)#replace space in city's name with %20,url can't have space
+    url = api_url.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description":parsed["weather"][0]["description"],
+                    "temperature":parsed["main"]["temp"],"city":parsed["name"]}
+    return weather
+
 @app.route("/")
 def get_news():
     query = request.args.get("publication")
@@ -17,7 +32,8 @@ def get_news():
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html", articles = feed['entries'])
+    weather = get_weather("London,UK")
+    return render_template("home.html", articles = feed['entries'], weather=weather)
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
