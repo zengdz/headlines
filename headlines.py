e264f9ac969da72c9d4092180ea2188ff7f0823e
zengdz <zeng.zhen@foxmail.com>
2016-10-11 14:42:52 +0800

    使用 cookies 记录用户输入信息(cookie)
    cookies
    本例用来记录用户的输入信息，没有用来记录用户身份。从当前用户输入还是cookie记录还是代码默认设置获取输入信息，使用一个函数来处理。

--- headlines.py
+++ headlines.py
@@ -1,9 +1,12 @@
 #For Python2.X
+#coding: utf-8
+import datetime # built-in mudule
 import json # built-in mudule
 import urllib2 # built-in mudule
 import urllib  # built-in mudule
-import feedparser
+import feedparser #解析RSS源
 from flask import Flask
+from flask import make_response
 from flask import render_template
 from flask import request
 
@@ -23,27 +26,36 @@ CURRENCY_URL ="https://openexchangerates.org//api/latest.json?app_id=53dc108e313
 @app.route("/")
 def home():
     # get customized headlines, based on user input or default
-    publication = request.args.get("publication")
-    if not publication:
-        publication = DEFAULTS['publication']
+    publication = get_value_fallback("publication")
     articles = get_news(publication)
+
     # get customized weather based on user input or default
-    city = request.args.get("city")
-    if not city:
-        city = DEFAULTS['city']
+    city = get_value_fallback("city")
     weather = get_weather(city)
+
     # get customized currency based on user input or default
-    currency_from = request.args.get("currency_from")
-    if not currency_from:
-        currency_from = DEFAULTS['currency_from']
-    currency_to = request.args.get("currency_to")
-    if not currency_to:
-        currency_to = DEFAULTS['currency_to']
+    currency_from = get_value_fallback("currency_from")
+    currency_to = get_value_fallback("currency_to")
     rate, currencies = get_rate(currency_from, currency_to)
     
-    return render_template("home.html", articles = articles, weather=weather,
-                           currency_from=currency_from, currency_to=currency_to,
-                           rate=rate, currencies=sorted(currencies))
+    #save cookies and return template
+    response = make_response(render_template("home.html", 
+    						articles = articles, weather=weather,
+                            currency_from=currency_from, currency_to=currency_to,
+                            rate=rate, currencies=sorted(currencies)))
+    expires = datetime.datetime.now() + datetime.timedelta(days=365)
+    response.set_cookie("publication", publication, expires=expires)
+    response.set_cookie("city", city, expires=expires)
+    response.set_cookie("currency_from", currency_from, expires=expires)
+    response.set_cookie("currency_to", currency_to, expires=expires)
+    return response
+
+def get_value_fallback(key):
+	if request.args.get(key):
+		return request.args.get(key)
+	if request.cookies.get(key):
+		return request.cookies.get(key)
+	return DEFAULTS[key]
 
 def get_rate(frm, to):
     all_currency = urllib2.urlopen(CURRENCY_URL).read()
